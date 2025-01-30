0a343db6da6a709ad39885429ae9fbc37f040df8
Ravexi1 <raklo2281337@mail.ru>
2025-01-30 23:16:52 +0500

    Tiled terrain

--- survival/Assets/Tiled/survivaltiles.tsx
+++ survival/Assets/Tiled/survivaltiles.tsx
@@ -1,4 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.1" name="survivaltiles" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
  <image source="survivaltiles.png" width="256" height="256"/>
+ <wangsets>
+  <wangset name="Ground" type="corner" tile="-1">
+   <wangcolor name="Dirt" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="3" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="4" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="5" wangid="0,2,0,1,0,2,0,1"/>
+   <wangtile tileid="32" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="33" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="35" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="36" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="37" wangid="0,1,0,2,0,1,0,2"/>
+   <wangtile tileid="64" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="65" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="66" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="67" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
